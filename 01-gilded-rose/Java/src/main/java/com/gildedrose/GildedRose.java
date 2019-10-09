b5af7bcc1874853b07c5ef56f9731c90c12eb51e
arek-dabrowski <arekdbr97@gmail.com>
2019-10-09 11:49:53 +0200

    changed for loop and simplified code

--- 01-gilded-rose/Java/src/main/java/com/gildedrose/GildedRose.java
+++ 01-gilded-rose/Java/src/main/java/com/gildedrose/GildedRose.java
@@ -8,52 +8,53 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) { //all except aged brie and backstage passes
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) { //all except aged brie, backstage passes and sulfuras
-                        items[i].quality = items[i].quality - 1; //quality decrease
+
+        for(Item item : items){
+            if (!item.name.equals("Aged Brie")
+                    && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) { //all except aged brie and backstage passes
+                if (item.quality > 0) {
+                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) { //all except aged brie, backstage passes and sulfuras
+                        item.quality = item.quality - 1; //quality decrease
                     }
                 }
             } else { //aged brie and backstage passes
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) { //backstage passes rapid increase
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) { //backstage passes rapid increase
+                        if (item.sellIn < 11) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
                             }
                         }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                        if (item.sellIn < 6) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
                             }
                         }
                     }
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) { //decrease sell in for all except sulfuras
-                items[i].sellIn = items[i].sellIn - 1;
+            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) { //decrease sell in for all except sulfuras
+                item.sellIn = item.sellIn - 1;
             }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) { //all except aged brie
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) { //all except backstage passes
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) { //all except sulfuras quality decrease
-                                items[i].quality = items[i].quality - 1;
+            if (item.sellIn < 0) {
+                if (!item.name.equals("Aged Brie")) { //all except aged brie
+                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) { //all except backstage passes
+                        if (item.quality > 0) {
+                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) { //all except sulfuras quality decrease
+                                item.quality = item.quality - 1;
                             }
                         }
                     } else { //for backstage passes quality 0
-                        items[i].quality = items[i].quality - items[i].quality;
+                        item.quality = 0;
                     }
                 } else { //only aged brie quality increase
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
                     }
                 }
             }
